5d1eeded4c71d3828d08d0f4b33cac909706c2ad
ivanmkc@google.com <ivanmkc@google.com>
2023-04-05 17:12:35 -0400

    Fixed vite configs

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-function App() {
-  const [count, setCount] = useState(0)
+import * as React from "react";
+import { StyledEngineProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useAnalytics } from "use-analytics";
+
+import DemoWrapper from "common/components/DemoWrapper";
+import DemoSelection from "common/pages/DemoSelection";
+import { forecastingDemoInfo, matchingEngineDemoInfo } from "DemoInfo";
+
+const queryClient = new QueryClient();
+
+const AppRoutes = () => {
+  const location = useLocation();
+  const analytics = useAnalytics();
+
+  React.useEffect(() => {
+    analytics.page({
+      url: location.pathname,
+    });
+  }, [location, analytics]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<DemoSelection />} />
+      <Route path="/demos" element={<DemoSelection />} />
+      <Route
+        path="/demos/time-series-forecasting"
+        element={<DemoWrapper {...forecastingDemoInfo} />}
+      />
+      <Route
+        path="/demos/time-series-forecasting/new-forecast"
+        element={<DemoWrapper {...forecastingDemoInfo} initialTabIndex={1} />}
+      />
+      <Route
+        path="/demos/matching-engine"
+        element={<DemoWrapper {...matchingEngineDemoInfo} />}
+      />
+    </Routes>
+  );
+};
+
+interface AppProps {
+  children: React.ReactNode;
 }
 
-export default App
+export const LayoutWithRouter = ({ children }: AppProps) => {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <StyledEngineProvider injectFirst>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </StyledEngineProvider>
+    </Router>
+  );
+};
+
+export default () => {
+  return (
+    <LayoutWithRouter>
+      <AppRoutes />
+    </LayoutWithRouter>
+  );
+};
