b1690b4ebcb30adf1264adde48042afaa2a5c55a
ivanmkc@google.com <ivanmkc@google.com>
2023-04-07 21:15:11 -0400

    Converted architecture to Tailwind

--- frontend-vite/src/demos/unified-cloud-search/pages/Architecture.tsx
+++ frontend-vite/src/demos/unified-cloud-search/pages/Architecture.tsx
@@ -14,92 +14,109 @@
  * limitations under the License.
  */
 
-import { Container, Typography } from '@mui/material';
-import SvgIcon from '@mui/material/SvgIcon';
-import Grid from '@mui/material/Unstable_Grid2';
 import { ReactComponent as ArchitectureImage } from 'demos/unified-cloud-search/static/architecture.svg';
 import GitHubButton from 'react-github-btn';
 
 export default () => (
-  <Container maxWidth="xl">
-    <Grid container spacing={7}>
-      <Grid md={12} lg={4}>
-        <Typography variant="h6">Architecture</Typography>
-        <Typography variant="body1">
-          The demo consists of a React frontend served via nginx and hosted on Cloud Run.
-        </Typography>
-        <br />
-        <Typography variant="overline">Frontend</Typography>
-        <Typography variant="body2">
-          The frontend is built using React and Material UI. It is served using NGINX. This is then containerized and
-          served on{' '}
-          <a
-            href="https://cloud.google.com/run/docs/quickstarts/build-and-deploy/deploy-python-service"
-            target="_blank"
-            rel="noreferrer"
+  <div className="container mx-auto max-w-7xl">
+    <div className="grid grid-cols-12 gap-7">
+      <div className="col-span-12 lg:col-span-4">
+        <article className="prose">
+          <h6 className="text-lg font-semibold">Architecture</h6>
+          <p className="text-base">The demo consists of a React frontend served via nginx and hosted on Cloud Run.</p>
+          <span className="uppercase text-xs">Frontend</span>
+          <p className="text-sm">
+            The frontend is built using React and TailwindCSS. It is served using NGINX. This is then containerized and
+            served on{' '}
+            <a
+              href="https://cloud.google.com/run/docs/quickstarts/build-and-deploy/deploy-python-service"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Cloud Run
+            </a>
+          </p>
+          <span className="uppercase text-xs">Backend</span>
+          <p className="text-sm">
+            The backend is a Python app served using FastAPI. It uses a threadpool to handle multiple training requests
+            in parallel. This is then containerized and deployed on Cloud Run.
+          </p>
+          <span className="uppercase text-xs">Similarity Search</span>
+          <p className="text-sm">
+            The demo uses{' '}
+            <a
+              href="https://cloud.google.com/vertex-ai/docs/matching-engine/overview"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Matching Engine
+            </a>{' '}
+            to do vector similarity search at scale. Matching Engine requires a{' '}
+            <a
+              href="https://cloud.google.com/vpc/docs/vpc"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              VPC network
+            </a>{' '}
+            to perform search. Cloud Run connects to the VPC network by using a{' '}
+            <a
+              href="https://cloud.google.com/vpc/docs/serverless-vpc-access"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Serverless VPC connector
+            </a>
+          </p>
+          <span className="uppercase text-xs">Memorystore</span>
+          <p className="text-sm">
+            <a
+              href="https://cloud.google.com/memorystore/docs/redis/redis-overview"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Memorystore
+            </a>{' '}
+            is a low-latency key-value database used to hydrate data that is retrieved from Matching Engine. For
+            example, given a StackOverflow question id, Memorystore is used to retrieve the question title.
+          </p>
+          <span className="uppercase text-xs">Cloud Vision</span>
+          <p className="text-sm">
+            <a
+              href="https://cloud.google.com/vision"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Cloud Vision
+            </a>{' '}
+            is used to filter out explicit images for the text-to-image dataset.
+          </p>
+          <h6 className="text-lg font-semibold">Deploy</h6>
+          <p className="text-base">See the Github repo for deployment instructions</p>
+          <GitHubButton
+            href="https://github.com/googlecloudplatform/google-cloud-ai-demos"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star googlecloudplatform/google-cloud-ai-demos on GitHub"
           >
-            Cloud Run
-          </a>
-        </Typography>
-        <br />
-        <Typography variant="overline">Backend</Typography>
-        <Typography variant="body2">
-          The backend is a Python app served using FastAPI. It uses a threadpool to handle multiple training requests in
-          parallel. This is then containerized and deployed on Cloud Run.
-        </Typography>
-        <br />
-        <Typography variant="overline">Similarity Search</Typography>
-        <Typography variant="body2">
-          The demo uses{' '}
-          <a href="https://cloud.google.com/vertex-ai/docs/matching-engine/overview" target="_blank" rel="noreferrer">
-            Matching Engine
-          </a>{' '}
-          to do vector similarity search at scale. Matching Engine requires a{' '}
-          <a href="https://cloud.google.com/vpc/docs/vpc" target="_blank" rel="noreferrer">
-            VPC network
-          </a>{' '}
-          to perform search. Cloud Run connects to the VPC network by using a{' '}
-          <a href="https://cloud.google.com/vpc/docs/serverless-vpc-access" target="_blank" rel="noreferrer">
-            Serverless VPC connector
-          </a>
-        </Typography>
-        <br />
-        <Typography variant="overline">Memorystore</Typography>
-        <Typography variant="body2">
-          <a href="https://cloud.google.com/memorystore/docs/redis/redis-overview" target="_blank" rel="noreferrer">
-            Memorystore
-          </a>{' '}
-          is a low-latency key-value database used to hydrate data that is retrieved from Matching Engine. For example,
-          given a StackOverflow question id, Memorystore is used to retrieve the question title.
-        </Typography>
-        <br />
-        <Typography variant="overline">Cloud Vision</Typography>
-        <Typography variant="body2">
-          <a href="https://cloud.google.com/vision" target="_blank" rel="noreferrer">
-            Cloud Vision
-          </a>{' '}
-          is used to filter out explicit images for the text-to-image dataset.
-        </Typography>
-        <br />
-        <Typography variant="h6">Deploy</Typography>
-        <Typography variant="body1">See the Github repo for deployment instructions</Typography>
-        <GitHubButton
-          href="https://github.com/googlecloudplatform/google-cloud-ai-demos"
-          data-size="large"
-          data-show-count="true"
-          aria-label="Star googlecloudplatform/google-cloud-ai-demos on GitHub"
-        >
-          Star
-        </GitHubButton>
-      </Grid>
-      <Grid md={12} lg={8}>
-        <SvgIcon
-          onClick={() => window.open('demos/unified-cloud-search/static/architecture.svg', '_blank')}
-          component={ArchitectureImage}
-          viewBox="0 0 2240 920"
-          sx={{ minHeight: '500px', width: '100%' }}
-        />
-      </Grid>
-    </Grid>
-  </Container>
+            Star
+          </GitHubButton>
+        </article>
+      </div>
+      <div className="col-span-12 lg:col-span-8">
+        <div className="flex">
+          <ArchitectureImage
+            onClick={() => window.open('demos/unified-cloud-search/static/architecture.svg', '_blank')}
+            className="h-96"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 );
