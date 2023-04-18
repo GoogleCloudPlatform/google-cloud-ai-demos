1254497bc49e38973961e2790fedf6d4bd67b0a9
ivanmkc@google.com <ivanmkc@google.com>
2023-04-18 18:00:10 -0400

    Made tracer_helper a singleton

--- backend/matching-engine/tracer_helper.py
+++ backend/matching-engine/tracer_helper.py
@@ -2,13 +2,28 @@ from opentelemetry import trace
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.trace import Link, Tracer
+from opentelemetry.trace import Tracer
+
+
+class SingletonTracerProvider:
+    _instance = None
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls._create_tracer_provider()
+        return cls._instance
+
+    @staticmethod
+    def _create_tracer_provider():
+        tracer_provider = TracerProvider()
+        cloud_trace_exporter = CloudTraceSpanExporter()
+        tracer_provider.add_span_processor(BatchSpanProcessor(cloud_trace_exporter))
+        trace.set_tracer_provider(tracer_provider)
+        return tracer_provider
 
 
 def get_tracer(instrumenting_module_name: str) -> Tracer:
-    tracer_provider = TracerProvider()
-    cloud_trace_exporter = CloudTraceSpanExporter()
-    tracer_provider.add_span_processor(BatchSpanProcessor(cloud_trace_exporter))
-    trace.set_tracer_provider(tracer_provider)
-
-    return trace.get_tracer(instrumenting_module_name)
+    return trace.get_tracer(
+        instrumenting_module_name, tracer_provider=SingletonTracerProvider.instance()
+    )
